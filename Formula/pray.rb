3fe78e04a41d8fa7cea78777f514040dbc026d99
Andrei Makarov <andrei@kiskolabs.com>
2026-07-29 13:11:37 +0300

    Bump pray to v1.6.0

--- Formula/pray.rb
+++ Formula/pray.rb
@@ -5,10 +5,10 @@
 class Pray < Formula
   desc "Reference CLI for Prayfile — package manager for pre-inference input"
   homepage "https://pray.kisko.dev"
-  url "https://github.com/kiskolabs/pray/archive/refs/tags/v1.5.1.tar.gz"
+  url "https://github.com/kiskolabs/pray/archive/refs/tags/v1.6.0.tar.gz"
   # Fill after tagged release:
   # shasum -a 256 <(curl -sL https://github.com/kiskolabs/pray/archive/refs/tags/v1.2.0.tar.gz)
-  sha256 "9294b1ccb1445f67013314f422157c0d6dae8dcece01fdcc6e7a709f310d3ca3"
+  sha256 "a0152288fb3b2538fc0fa9d0882846c2839bdd01de161e254f9b8740ff887ef9"
   license "MIT"
   head "https://github.com/kiskolabs/pray.git", branch: "main"
 
